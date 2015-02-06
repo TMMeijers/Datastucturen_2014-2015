d98e7cfa79d08a7dd3082daab29d16134f3f6a6f
TMMeijers <t.m.meijers@Hotmail.com>
2015-02-06 17:47:49 +0100

    main comment assignment1.java to do

--- assignment1/code/Assignment1.java
+++ assignment1/code/Assignment1.java
@@ -5,6 +5,8 @@ import java.lang.String;
  * Example solution for the first assignment of the course Data Structures 2009/2010 at the University of Amsterdam. 
  * Times the mutations (population and depopulation) of various data structures provided by the Java standard library 
  * that either implement the List or Queue interface.
+ * <p>
+ * TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO 
  */
 public class Assignment1 {
 
