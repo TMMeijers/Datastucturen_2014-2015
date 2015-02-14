398f40a5b3bed37e8bc34ecee173cb5ddc359098
markus <mama@mia.it>
2015-02-14 13:55:51 +0100

    check input, fixed output

--- assignment_2/code/OwnSpellChecker.java
+++ assignment_2/code/OwnSpellChecker.java
@@ -3,8 +3,6 @@ import java.io.*;
 class OwnSpellChecker {
     public static void main(String[] args) {
         int hash_size;
-        int count = 0, typo = 0;
-        long start = 0, end = 0;
         String wordfile, textfile;
         Compressable function;
         Strategy strategy;
@@ -12,13 +10,29 @@ class OwnSpellChecker {
         /* Shared token to store for every word in the hash table. */
 
         if (!(args.length == 3) ) {
-            System.out.println("Usage: java SpellChecker <wordfile> <text> <size>");
-            System.exit(0);
+            System.err.println("Usage: java SpellChecker <wordfile> <textfile> <table_size>");
+            System.exit(1); // on error
         }
         wordfile = args[0];
         textfile = args[1];
         hash_size = Integer.parseInt(args[2]);
-        System.out.printf("Selected table size: %d\n", hash_size);
+
+        if (hash_size <= 0) {
+            System.err.printf("<table_size> must be bigger than 0.\n");
+            System.exit(1);
+        }
+        File wordFileHandle = new File(wordfile);
+        File textFileHandle = new File(textfile);
+        if (!wordFileHandle.exists() || wordFileHandle.isDirectory()) {
+            System.err.println("<wordfile> doesn't exist or is a directory\n");
+            System.exit(1);
+        }
+        if (!textFileHandle.exists() || textFileHandle.isDirectory()) {
+            System.err.println("<textfile> doesn't exist or is a directory\n");
+            System.exit(1);
+        }
+        
+        System.out.printf("Selected table size: %d\n", hash_size);                
        
         // Make list to test all implementations
         function = new DivisionHasher(hash_size);
@@ -30,12 +44,17 @@ class OwnSpellChecker {
         strategy = new QuadraticProbing(hash_size);
         tables.add(new OpenHashtable(hash_size, function, strategy));
         // Table with double hashing
-        // strategy = new DoubleHashProbing(hash_size);
-        // tables.add(new OpenHashtable(hash_size, function, strategy));
+        //strategy = new DoubleHashProbing(hash_size);
+        //tables.add(new OpenHashtable(hash_size, function, strategy));
         // Table with collision chaining
         tables.add(new ChainHashtable(hash_size, function));
 
         for (AbstractHashtable table : tables) {
+            long start = 0;
+            long end = 0;
+            int count = 0;
+            int typo = 0;
+
             System.out.println("\nTesting Hashtable with \"" + table.printStrategy() + "\" collision prevention");
             /* Read wordfile, and insert every word into the hash table. */
             try {
@@ -50,11 +69,13 @@ class OwnSpellChecker {
                 in.close();
             } catch (IOException e) {
                 e.printStackTrace();
+                System.exit(1);
             }
             System.out.println("Hash table built in " + (end - start) + " ms");
 
             // Read text file, and lookup every word in the hash table.
             try {
+                
                 BufferedReader src = new BufferedReader(new FileReader(textfile));
                 start = System.currentTimeMillis();
                 String str = src.readLine();
@@ -72,8 +93,11 @@ class OwnSpellChecker {
                     str = src.readLine();
                 }
                 end = System.currentTimeMillis();
+                
+
             } catch (IOException e) {
                 e.printStackTrace();
+                System.exit(1);
             }
 
             System.out.printf("Hash table contains %d words\n", table.size());
